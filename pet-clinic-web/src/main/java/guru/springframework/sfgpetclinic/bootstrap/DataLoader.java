b4fcbd635b32950b636d5428d67f897657e513bf
Anbarasu1982 <anbarasu1982@gmail.com>
2021-06-21 10:08:53 +0530

    Adding Contact Info and Pet details to Owner entity. Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.service.OwnerService;
@@ -10,6 +11,8 @@ import guru.springframework.sfgpetclinic.service.map.OwnerServiceMap;
 import guru.springframework.sfgpetclinic.service.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -42,12 +45,34 @@ public class DataLoader implements CommandLineRunner {
             Owner owner1 = new Owner();
             owner1.setFirstName("Anbarasu");
             owner1.setLastName("Shanmugham");
+            owner1.setAddress("12 Vellai Pillai Street");
+            owner1.setCity("Chennai");
+            owner1.setTelephone("+919840995982");
+
+            Pet anbusPet = new Pet();
+            anbusPet.setName("Jacky");
+            anbusPet.setPetType(savedDogPetType);
+            anbusPet.setBirthDate(LocalDate.now());
+            anbusPet.setOwner(owner1);
+
+            owner1.getPets().add(anbusPet);
 
             ownerService.save(owner1);
 
             Owner owner2 = new Owner();
             owner2.setFirstName("Kiruthiga");
             owner2.setLastName("Anbarasu");
+            owner2.setAddress("12 Vellai Pillai Nagar");
+            owner2.setCity("Chennai");
+            owner2.setTelephone("+919840158079");
+
+            Pet kirusPet = new Pet();
+            kirusPet.setName("Claris");
+            kirusPet.setPetType(savedCatPetType);
+            kirusPet.setBirthDate(LocalDate.now());
+            kirusPet.setOwner(owner2);
+
+            owner2.getPets().add(kirusPet);
 
             ownerService.save(owner2);
             System.out.println("Created Owners!!!");
