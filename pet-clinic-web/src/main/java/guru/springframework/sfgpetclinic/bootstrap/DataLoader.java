80ed2eb69c4fe9f1831bfcbae80123ebd4d40fdc
Anbarasu1982 <anbarasu1982@gmail.com>
2021-06-30 23:20:08 +0530

    Load Visits during Startup Bootstrap. Closes #48

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.*;
-import guru.springframework.sfgpetclinic.service.OwnerService;
-import guru.springframework.sfgpetclinic.service.PetTypeService;
-import guru.springframework.sfgpetclinic.service.SpecialityService;
-import guru.springframework.sfgpetclinic.service.VetService;
+import guru.springframework.sfgpetclinic.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -74,6 +73,15 @@ public class DataLoader implements CommandLineRunner {
 
             ownerService.save(owner2);
             System.out.println("Created Owners!!!");
+
+            Visit catVisit = new Visit();
+            catVisit.setDescription("Sneezy Devil");
+            catVisit.setPet(kirusPet);
+            catVisit.setDateOfVisit(LocalDate.now());
+
+            visitService.save(catVisit);
+
+            System.out.println("Created Cat Visit!!!");
 
             Speciality radiology = new Speciality();
             radiology.setDescription("Radiology");
